b70dd434b65e29aae2b36d5905214bab40a20f05
SAI GANESH DOKALA <61933709+saiganesh-d@users.noreply.github.com>
2021-06-27 10:26:02 +0530

    Update urls.py

--- welcome/urls.py
+++ welcome/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path("index", views.index, name="index"),
+    path("", views.index, name="index"),
     path("about",views.about, name="about"),
     path("skils",views.skills,name="skills"),
     path("projects",views.projects,name="projects")
