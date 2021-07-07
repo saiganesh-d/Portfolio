7dff8a7a7d39f30c813cf0d52297227e6eee1e37
saiganesh-d <saiganesh94dokala@gmail.com>
2021-07-07 21:33:01 +0530

    sa2

--- welcome/urls.py
+++ welcome/urls.py
@@ -3,8 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path("", views.index, name="index"),
+    path("index", views.index, name="index"),
     path("about",views.about, name="about"),
     path("skils",views.skills,name="skills"),
-    path("projects",views.projects,name="projects")
+    path("projects",views.projects,name="projects"),
+    path("cars",views.cars,name="cars"),
 ]
--- welcome/views.py
+++ welcome/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.urls import reverse
+import joblib
+import numpy as np
+from django.shortcuts import HttpResponse, HttpResponseRedirect,render
 
 
 
@@ -25,3 +28,50 @@ def projects(request):
     return render(request, "welcome/projects.html", {
         "name" : "projects"
     })
+
+
+def cars(request):
+    rf=joblib.load('final.sav')
+    if request.method == "GET":
+        return render(request,"welcome/cars.html")
+
+    
+       
+    Year =request.POST['Year']
+    Present_Price =request.POST['Present_Price']
+    Kms_Driven =request.POST['Kms_Driven']
+    Owner =request.POST['Owner']
+    Fuel_Type_Petrol =request.POST['Fuel_Type_Petrol']
+    Seller_Type_Individual =request.POST['Seller_Type_Individual']
+    Transmission_Mannual =request.POST['Transmission_Mannual']
+    Year= int(Year)
+    Year= 2021-Year
+    Present_Price=float(Present_Price)
+    
+    if(Fuel_Type_Petrol=='Petrol'):
+        Fuel_Type_Petrol=1
+        Fuel_Type_Diesel=0
+    else:
+        Fuel_Type_Petrol=0
+        Fuel_Type_Diesel=1
+    if(Seller_Type_Individual=='Individual'):
+        Seller_Type_Individual=1
+    else:
+        Seller_Type_Individual=0
+    if(Transmission_Mannual=='Mannual'):
+        Transmission_Mannual=1
+    else:
+        Transmission_Mannual=0
+
+
+
+    prediction=rf.predict([[Year,Present_Price,Kms_Driven,Owner,Fuel_Type_Diesel,Fuel_Type_Petrol,Seller_Type_Individual,Transmission_Mannual]])
+    output=round(prediction[0],2)
+    if output<0:
+        return render(request,"welcome/cars.html",{"prediction_texts":"Sorry you cannot sell this car"})
+    else:
+            return render(request,"welcome/cars.html",{"prediction_text":"You Can Sell The Car at {}".format(output)})
+    
+       
+
+    
